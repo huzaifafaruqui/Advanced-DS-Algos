199d6591bb76801d9691eeba63d102c3f5a9f02a
huzaifafaruqui <huzaifafaruqui@outlook.com>
2017-06-23 02:42:14 +0530

    search added

--- AVL/AVL.h
+++ AVL/AVL.h
@@ -21,7 +21,7 @@ private:
     };
     size_t count = 0;
     std::shared_ptr<AVLNode> root;
-    int getHeight(std::shared_ptr<AVLNode>& p)
+    unsigned int getHeight(std::shared_ptr<AVLNode>& p)
     {
         if(p)
             return p->height;
@@ -30,20 +30,24 @@ private:
 
     std::shared_ptr<AVLNode> insert(T, std::shared_ptr<AVLNode>&);
     //https://stackoverflow.com/questions/10643563/how-to-return-smart-pointers-shared-ptr-by-reference-or-by-value
-    bool remove(T, std::shared_ptr<AVLNode>&);
+    void remove(std::shared_ptr<AVLNode>&);
     //shared pointers are heavy objects that should be passed by reference
-    std::shared_ptr<AVLNode> find(T, std::shared_ptr<AVLNode>&);
+    std::shared_ptr<AVLNode> search(T, std::shared_ptr<AVLNode>&);
+
+    std::shared_ptr<AVLNode> extract_min(std::shared_ptr<AVLNode>&);
+
     void adjustBalanceFactors(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
     void rotateLeft(std::shared_ptr<AVLNode>&);
     void rotateRight(std::shared_ptr<AVLNode>&);
     void adjustLeftRight(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
     void adjustRightLeft(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
+    void printInorder(std::ostream&, std::shared_ptr<AVLNode>&);
     
 public:
     AVLTree():root(){}
     ~AVLTree() {
-        //while(root)
-          //  remove(root->data);
+        while(root)
+            remove(root->data);
     }
     AVLTree(AVLTree&&) = default;
     AVLTree& operator =(AVLTree&&) = default;
@@ -51,10 +55,11 @@ public:
     AVLTree& operator =(const AVLTree&);
     bool insert(T);
     bool remove(T);
-    void inorder();
-    bool search(T x) {
-        return !(find(x, root)==nullptr);
+    unsigned int height()
+    {
+        return getHeight(root);
     }
+    bool search(T);
     size_t size(){
         return count;
     }
--- AVL/AVLTree.cpp
+++ AVL/AVLTree.cpp
@@ -26,11 +26,103 @@ std::shared_ptr<typename AVLTree<T>::AVLNode> AVLTree<T>::insert(T val, std::sha
         p->right = insert(val, p->right);
     }
 
-    unsigned int hr = getheight(p->right);
-    unsigned int hl = getheight(p->left);
-
-    p->height = std::max(hr, hl) + 1;
-
 
     return p;
+}
+
+
+template<typename T>
+bool AVLTree<T>::search(T val) 
+{
+    if(search(val, root))
+        return true;
+    return false;
+}
+
+
+template<typename T>
+std::shared_ptr<typename AVLTree<T>::AVLNode> AVLTree<T>::search(T val, std::shared_ptr<AVLNode>& p)
+{
+    if(p == nullptr || p->data == val)
+    {
+        return p;
+    }
+
+    else if(val < p->data)
+    {
+        return search(val, p->left);
+    }
+
+    return search(val, p->right);
+
+}
+
+
+template<typename T>
+std::shared_ptr<typename AVLTree<T>::AVLNode> AVLTree<T>::extract_min(std::shared_ptr<AVLNode>& p)
+{
+    if(p->left) //make sure p!=nullptr
+    {
+        return extract_min(p->left);
+    }
+
+    return p;
+
+}
+
+
+template<typename T>
+bool AVLTree<T>::remove(T val) 
+{
+    auto temp = search(val, root);
+    if(temp)
+    {
+        remove(temp);
+        return true;
+    }
+    return false;
+}
+
+
+template<typename T>
+void AVLTree<T>::remove(std::shared_ptr<AVLNode>& p)
+{
+    if(p->left == nullptr)
+    {
+        p = std::move(p->right);
+        //https://stackoverflow.com/questions/41871115/why-would-i-stdmove-an-stdshared-ptr
+    }
+
+    else if(p->right == nullptr)
+    {
+        p = std::move(p->left);
+    }
+
+    else
+    {
+        auto temp = extract_min(p->right);
+        p->data = temp->data;
+        remove(temp);
+    }
+
+
+}
+
+template<typename T>
+void printInorder(std::ostream& out, std::shared_ptr<typename AVLTree<T>::AVLNode>& p)
+{
+    if(p)
+    {
+        printInorder(out, p->left);
+        out << p->data;
+        printInorder(out, p->right);
+    }
+}
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const AVLTree<T>& A) //TODO
+{
+    A.printInorder(out, A.root);
+    return out;
 }
