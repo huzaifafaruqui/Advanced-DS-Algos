69bcc2d62f0fc499d602f4c4cc1283d83481f8fa
huzaifafaruqui <huzaifafaruqui@outlook.com>
2017-06-23 02:47:54 +0530

    code formatted

--- AVL/AVL.h
+++ AVL/AVL.h
@@ -1,76 +1,62 @@
 #ifndef AVL_H
 #define AVL_H
-//https://en.wikipedia.org/wiki/Include_guard
+// https://en.wikipedia.org/wiki/Include_guard
 
 #include <memory>
 
+template <typename T>
+class AVLTree {
+ private:
+  struct AVLNode {
+    T data;
+    std::shared_ptr<AVLNode> left;
+    std::shared_ptr<AVLNode> right;
+    unsigned int height;
+    AVLNode() : data(), left(), right(), height() {}
+    AVLNode(T val) : data(val), left(), right(), height() {}
+  };
+  size_t count = 0;
+  std::shared_ptr<AVLNode> root;
+  unsigned int getHeight(std::shared_ptr<AVLNode>& p) {
+    if (p) return p->height;
+    return 0;
+  }
 
+  std::shared_ptr<AVLNode> insert(T, std::shared_ptr<AVLNode>&);
+  // https://stackoverflow.com/questions/10643563/how-to-return-smart-pointers-shared-ptr-by-reference-or-by-value
+  void remove(std::shared_ptr<AVLNode>&);
+  // shared pointers are heavy objects that should be passed by reference
+  std::shared_ptr<AVLNode> search(T, std::shared_ptr<AVLNode>&);
 
-template<typename T>
-class AVLTree
-{
-private:
-    struct AVLNode
-    {
-        T data;
-        std::shared_ptr<AVLNode> left;
-        std::shared_ptr<AVLNode> right;
-        unsigned int height;
-        AVLNode() : data(), left(), right(), height() {}
-        AVLNode(T val) : data(val), left(), right(), height() {}
-    };
-    size_t count = 0;
-    std::shared_ptr<AVLNode> root;
-    unsigned int getHeight(std::shared_ptr<AVLNode>& p)
-    {
-        if(p)
-            return p->height;
-        return 0;
-    }
+  std::shared_ptr<AVLNode> extract_min(std::shared_ptr<AVLNode>&);
 
-    std::shared_ptr<AVLNode> insert(T, std::shared_ptr<AVLNode>&);
-    //https://stackoverflow.com/questions/10643563/how-to-return-smart-pointers-shared-ptr-by-reference-or-by-value
-    void remove(std::shared_ptr<AVLNode>&);
-    //shared pointers are heavy objects that should be passed by reference
-    std::shared_ptr<AVLNode> search(T, std::shared_ptr<AVLNode>&);
+  void adjustBalanceFactors(std::shared_ptr<AVLNode>&,
+                            std::shared_ptr<AVLNode>&);
+  void rotateLeft(std::shared_ptr<AVLNode>&);
+  void rotateRight(std::shared_ptr<AVLNode>&);
+  void adjustLeftRight(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
+  void adjustRightLeft(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
+  void printInorder(std::ostream&, std::shared_ptr<AVLNode>&);
 
-    std::shared_ptr<AVLNode> extract_min(std::shared_ptr<AVLNode>&);
+ public:
+  AVLTree() : root() {}
+  ~AVLTree() {
+    while (root) remove(root->data);
+  }
+  AVLTree(AVLTree&&) = default;
+  AVLTree& operator=(AVLTree&&) = default;
+  AVLTree(const AVLTree&);
+  AVLTree& operator=(const AVLTree&);
+  bool insert(T);
+  bool remove(T);
+  unsigned int height() { return getHeight(root); }
+  bool search(T);
+  size_t size() { return count; }
+  // void restoreAVL(AVLTreeNode *ancestor, AVLTreeNode *newNode);
 
-    void adjustBalanceFactors(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
-    void rotateLeft(std::shared_ptr<AVLNode>&);
-    void rotateRight(std::shared_ptr<AVLNode>&);
-    void adjustLeftRight(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
-    void adjustRightLeft(std::shared_ptr<AVLNode>&, std::shared_ptr<AVLNode>&);
-    void printInorder(std::ostream&, std::shared_ptr<AVLNode>&);
-    
-public:
-    AVLTree():root(){}
-    ~AVLTree() {
-        while(root)
-            remove(root->data);
-    }
-    AVLTree(AVLTree&&) = default;
-    AVLTree& operator =(AVLTree&&) = default;
-    AVLTree(const AVLTree&);
-    AVLTree& operator =(const AVLTree&);
-    bool insert(T);
-    bool remove(T);
-    unsigned int height()
-    {
-        return getHeight(root);
-    }
-    bool search(T);
-    size_t size(){
-        return count;
-    }
-    //void restoreAVL(AVLTreeNode *ancestor, AVLTreeNode *newNode);
-    
-    template<class U>
-    friend std::ostream& operator<<(std::ostream&, const AVLTree<U>& );
-
+  template <class U>
+  friend std::ostream& operator<<(std::ostream&, const AVLTree<U>&);
 };
-
-
 
 #include "AVLTree.cpp"
 
